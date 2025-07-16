6017cfb9f2457161c9551a0c272c091c7d92b5b9
Abhishek kumar <abhisehkabd4747@gmail.com>
2025-07-16 22:45:35 +0530

    new code

---
+++ validparenthese.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdbool.h>
+bool validString(char*s){
+  int low=0;int high=0;
+
+  for(int i=0;s[i]!='\0';i++){
+    if(s[i]=='('){
+        low++;
+        high++;
+    }else if(s[i]==')'){
+        if(low>0){
+            low--;
+            high--;
+        }
+    }else if(s[i]=='*'){
+        if(low>0){
+            low--;
+            high++;
+        }
+    }
+    if(high<0) return false;
+    
+  }
+  return low==0;
+
+}
+
+int main(){
+printf("%d\n",validString("(*)"));
+}
---
+++ versin.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<String.h>
+#include<stdlib.h>
+
+int version(const char *version1,const char *version2){
+char *v1=strdup(version1);
+char *v2=strdup(version2);
+
+char *token1=strtok(v1,".");
+char *token2=strtok(v2,".");
+while(token1!= NULL || token2 != NULL){
+    int num1=token1?atoi(token1):0;
+    int num2=token2?atoi(token2):0;
+
+    if(num1>num2){
+        free(v1);
+        free(v2);
+        return 1;
+    }else if(num1<num2){
+        free(v1);
+        free(v2);
+        return -1;
+    }
+    token1=strtok(NULL,".");
+    token2=strtok(NULL,".");
+    return 0;
+}
+
+}
+int main(){
+printf("%d\n",version("1.01","1.001"));
+}
